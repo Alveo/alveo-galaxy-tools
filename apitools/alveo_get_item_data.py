09ca38a9363b634f3b995f40342ea58e5a0e4b96
Steve Cassidy <steve.cassidy@mq.edu.au>
2017-02-07 15:28:50 +1100

    use file base name as the designation, without extension, this should then be carried through any analysis steps

--- apitools/alveo_get_item_data.py
+++ apitools/alveo_get_item_data.py
@@ -46,7 +46,7 @@ def galaxy_name(itemname, fname):
 
     root, ext = os.path.splitext(fname)
     ext = ext[1:] # remove initial .
-    fname = FNPAT % {'designation': fname, 'ext': ext}
+    fname = FNPAT % {'designation': root, 'ext': ext}
 
     return fname
 
