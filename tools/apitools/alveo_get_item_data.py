53cde5cc9b8c1adcccdc3cfa52d8ca82079aeda7
Steve Cassidy <steve.cassidy@mq.edu.au>
2017-12-15 16:59:45 +1100

    Consolidate get-data tools into a single tool, remove unused tools.

--- tools/apitools/alveo_get_item_data.py
+++ tools/apitools/alveo_get_item_data.py
@@ -11,7 +11,8 @@ def parser():
     p = argparse.ArgumentParser(description="Downloads documents in an Alveo Item List")
     p.add_argument('--api_key', required=True, action="store", type=str, help="Alveo API key")
     p.add_argument('--item_list', required=True, action="store", type=str, help="File containing list of item URLs")
-    p.add_argument('--patterns', required=True, action="store", type=str, help="File patterns to download")
+    p.add_argument('--patterns', required=False, action="store", type=str, help="File patterns to download")
+    p.add_argument('--primarytext', required=False, action="store_true", help="Download only primary text")
     p.add_argument('--output_path', required=True, action="store", type=str, help="Path to output file")
     return p.parse_args()
 
@@ -20,25 +21,24 @@ def parser():
 FNPAT = "%(designation)s#%(ext)s"
 
 
-def galaxy_name(itemname, fname):
-    """construct a filename suitable for Galaxy dataset discovery
-    designation - (dataset identifier) is the file basename
-    ext - defines the dataset type and is the file extension
-    """
+def galaxy_name(fname, ext):
+    """construct a filename suitable for Galaxy dataset discovery"""
 
-    root, ext = os.path.splitext(fname)
-    ext = ext[1:]  # remove initial .
-    fname = FNPAT % {'designation': root, 'ext': ext}
+    fname = FNPAT % {'designation': fname, 'ext': ext}
 
     return fname
 
 
-def download_documents(item_list, patterns, output_path):
+def download_documents(item_list, output_path, patterns=None):
     """
-    Downloads a list of documents to the directory specificed by output_path.
+    Downloads a list of documents to the directory specified by output_path.
 
     :type documents: list of pyalveo.Document
     :param documents: Documents to download
+
+    :type patterns: string
+    :param patterns: Glob pattern for files to download, may include the special
+    pattern PRIMARYTEXT which returns the primary text of each item as ITEMNAME.txt
 
     :type output_path: String
     :param output_path: directory to download to the documents to
@@ -46,22 +46,31 @@ def download_documents(item_list, patterns, output_path):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    downloaded = []
-
     items = item_list.get_all()
     for item in items:
+        md = item.metadata()
+        ident = md['alveo:metadata']['dcterms:identifier']
+
         documents = item.get_documents()
         for doc in documents:
             for pattern in patterns:
-                if not pattern == '' and fnmatch(doc.get_filename(), pattern):
-                    fname = galaxy_name(item.metadata()['alveo:metadata']['dcterms:identifier'], doc.get_filename())
+                if pattern == 'PRIMARYTEXT':
+                    fname = os.path.join(output_path, galaxy_name(ident, 'txt'))
+                    content = item.get_primary_text()
+                    if content is not None:
+                        with open(fname, 'w') as out:
+                            out.write(content.decode('utf-8'))
+
+                elif not pattern == '' and fnmatch(doc.get_filename(), pattern):
+                    root, ext = os.path.splitext(doc.get_filename())
+                    ext = ext[1:]  # remove initial .
+                    fname = galaxy_name(root, ext)
                     try:
                         doc.download_content(dir_path=output_path, filename=fname)
-                        downloaded.append(doc.get_filename())
+
                     except pyalveo.APIError as e:
                         print("ERROR: " + str(e), file=sys.stderr)
                         sys.exit(1)
-    return downloaded
 
 
 def main():
@@ -72,8 +81,10 @@ def main():
         client = pyalveo.Client(api_url=API_URL, api_key=api_key, use_cache=False)
 
         item_list = read_item_list(args.item_list, client)
+
         patterns = args.patterns.split(',')
-        download_documents(item_list, patterns, args.output_path)
+        download_documents(item_list, args.output_path, patterns=patterns)
+
     except pyalveo.APIError as e:
         print("ERROR: " + str(e), file=sys.stderr)
         sys.exit(1)
